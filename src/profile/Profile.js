9723b638968b22be3d705ca1fe236db929fc9e35
Zach Nicholson <raiste.doctow@gmail.com>
2020-06-19 21:33:51 -0500

    skeleton built

---
+++ src/profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ApiManager from '../modules/ApiManager';
+
+const Profile = () => {
+    return (
+        <div className="profile_container">
+            <div className="profile_banner">Banner</div>
+            <div className="profile_stats">Stats</div>
+            <div className="profile_graphs">Graphs</div>
+        </div>
+    )
+}
+
+export default Profile
