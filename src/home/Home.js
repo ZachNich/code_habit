e444c95b6bfac6173a65d7afbc456b303a855f1f
Zach Nicholson <raiste.doctow@gmail.com>
2020-07-11 15:07:46 -0500

    separated upcoming problems into upcoming probs and due reviews, added/styled problem cards for this

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 import ApiManager from '../modules/ApiManager';
 import Prism from 'prismjs';
 import 'prismjs/themes/prism-xonokai.css';
+import ProblemCard from './ProblemCard';
 import mascot from '../media/mascot1.png';
 import './Home.css';
 
@@ -10,6 +11,8 @@ const Home = props => {
     const [recentSolution, setRecentSolution] = useState({})
     const [recentProblem, setRecentProblem] = useState({})
     const [upcomingProblems, setUpcomingProblems] = useState([])
+    const [dueReviews, setDueReviews] = useState([])
+    const [showProblems, setShowProblems] = useState(true)
 
     const getSolutions = () => {
         ApiManager.getByProperty('userSolutions', 'profileId', JSON.parse(sessionStorage.user).id)
@@ -35,8 +38,21 @@ const Home = props => {
             ApiManager.getAll('problems').then(problems => {
                 const unsolvedProblems = problems.filter(problem => !solutions.some(solution => solution.problemId === problem.id))
                 setUpcomingProblems(unsolvedProblems)
+                const dueReviews = solutions.filter(solution => Date.parse(solution.nextEncounterDate) <= Date.parse(new Date()))
+                const dontReviews = solutions.filter(solution => Date.parse(solution.nextEncounterDate) > Date.parse(new Date()))
+                const filteredReviews = dueReviews.filter(solution => !dontReviews.some(sol => solution.problemId === sol.problemId))
+                const reviewProblems = problems.filter(problem => filteredReviews.some(solution => solution.problemId === problem.id))
+                setDueReviews(reviewProblems)
             })
         })
+    }
+
+    const toggleShowProblems = e => {
+        if (showProblems) {
+            setShowProblems(false);
+        } else {
+            setShowProblems(true);
+        }
     }
 
     useEffect(() => {
@@ -56,8 +72,18 @@ const Home = props => {
                 <h4 className="home_header">Most Recent Solution</h4>
                 <h5 className="home_subheader">{recentProblem.title}</h5>
                 <pre className="home_content home_recentSolution"><code className="language-javascript">{recentSolution.description}</code></pre>
-                <h4 className="home_header">Preview Upcoming Problems</h4>
-                <div className="home_content home_upcomingProblems">{upcomingProblems.map(problem => <p key={problem.id} className="home_problem">{problem.description}</p>)}</div>
+                <h4 className="home_header preview_header">
+                    <p onClick={showProblems ? null : toggleShowProblems} className={showProblems ? "active_preview" : "preview"}>Preview Upcoming Problems</p>
+                    <p onClick={showProblems ? toggleShowProblems : null} className={showProblems ? "preview" : "active_preview"}>Preview Due Reviews</p>
+                </h4>
+                <div className="home_content home_upcomingProblems">
+                {showProblems ? upcomingProblems.map(problem => 
+                    <ProblemCard problem={problem} />
+                )
+                : dueReviews.map(review => 
+                    <ProblemCard problem={review} />
+                )
+                }</div>
             </div>
             <div className="home_rightside">
                 <aside className="home_quote">"Habits stay with you even when you don't have the motivation." <p className="home_quote_author">— Neeraj Agnihotri</p></aside>
---
+++ src/home/ProblemCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css';
+
+const ProblemCard = props => {
+    return (
+        <div className="problem_card" id={`problem-${props.problem.id}`}>
+            <p className="home_problem_title">{props.problem.title}</p>
+            <p className="home_problem_description">{props.problem.description}</p>
+        </div>
+    )
+}
+
+export default ProblemCard
