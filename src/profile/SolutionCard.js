6de9aa78b69d463ecbabbe95a0bb83a9e1da7d69
Zach Nicholson <raiste.doctow@gmail.com>
2020-07-15 21:15:00 -0500

    styled code for past sols on profile

--- src/profile/SolutionCard.js
+++ src/profile/SolutionCard.js
@@ -5,7 +5,9 @@ const SolutionCard = props => {
     return (
         <div className="problem_card" id={`solution-${props.solution[0].id}`}>
             <p className="profile_problem_title">{props.solution[1].title}</p>
-            <p className="profile_solution">{props.solution[0].description}</p>
+            <pre className="profile_solution_container">
+                <code className="profile_solution">{props.solution[0].description}</code>
+            </pre>
         </div>
     )
 }
