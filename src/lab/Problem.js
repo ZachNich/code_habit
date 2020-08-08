bddc92f237e6ea9cca3cfcef50b67ce6c660151e
Zach Nicholson <raiste.doctow@gmail.com>
2020-08-08 15:26:09 -0500

    problem titles added to labit/habit

--- src/lab/Problem.js
+++ src/lab/Problem.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 const Problem = props => {
     const [description, setDescription] = useState('')
+    const [title, setTitle] = useState('')
 
-    useEffect(() => setDescription(props.problem.description), [props.problem])
+    useEffect(() => {
+        setDescription(props.problem.description)
+        setTitle(props.problem.title)
+    }, [props.problem])
 
     return (
         <div className="problem_container">
             <h3 className="lab_header">Our Problem</h3>
+            <h5 className="lab_problem_title">{title}</h5>
             <p>{description}</p>
         </div>
     )
